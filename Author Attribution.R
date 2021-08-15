915d272580f4e4ad02d482f3c358f86f07f1dd71
Chaitra Setty <chaitra.setty604@gmail.com>
2021-08-15 15:15:45 -0500

    Author attribution with train

--- Author Attribution.R
+++ Author Attribution.R
@@ -23,6 +23,43 @@ readerPlain = function(fname){
             id=fname, language='en') }
 
 
+### TRAIN DATA #####
+author_dirs_train = Sys.glob('data/ReutersC50/C50train/*')
+
+file_list_train = NULL
+labels_train = NULL
+for(author_train in author_dirs_train) {
+  author_name_train = tail(strsplit(author_train,split="/")[[1]],1)
+  files_to_add_train = Sys.glob(paste0(author_train, '/*.txt'))
+  file_list_train = append(file_list_train, files_to_add_train)
+  labels_train = append(labels_train, rep(author_name_train, length(files_to_add_train)-1))
+}
+
+
+# Need a more clever regex to get better names here
+train_docs = lapply(file_list_train, readerPlain) 
+names(train_docs) = file_list_train
+names(train_docs) = sub('.txt', '', names(train_docs))
+my_corpus_train = Corpus(VectorSource(train_docs))
+
+my_corpus_train = tm_map(my_corpus_train, content_transformer(tolower)) # make everything lowercase
+my_corpus_train = tm_map(my_corpus_train, content_transformer(removeNumbers)) # remove numbers
+my_corpus_train = tm_map(my_corpus_train, content_transformer(removePunctuation)) # remove punctuation
+my_corpus_train = tm_map(my_corpus_train, content_transformer(stripWhitespace)) ## remove excess white-space
+my_corpus_train = tm_map(my_corpus_train, content_transformer(removeWords), stopwords("SMART"))
+
+DTM_train = DocumentTermMatrix(my_corpus_train)
+DTM_train # some basic summary statistics
+
+class(DTM_train)  # a special kind of sparse matrix format
+
+inspect(DTM_train[1:10,1:20])
+DTM_train = removeSparseTerms(DTM_train, 0.975)
+
+tfidf_train = weightTfIdf(DTM_train)
+train.data = as.data.frame(as.matrix(tfidf_train), stringsAsFactors=FALSE)
+
+
 ### TEST DATA #####
 author_dirs_test = Sys.glob('data/ReutersC50/C50test/*')
 
@@ -58,21 +95,44 @@ DTM_test = removeSparseTerms(DTM_test, 0.975)
 
 test.data = as.data.frame(as.matrix(DTM_test), stringsAsFactors=FALSE)
 
-
-###RANDOM FOREST###
-library(randomForest)
-set.seed(1234)
-common_cols = intersect(names(train.data), names(test.data))
-X_train =train.data[,c(common_cols)]
-train_rf = randomForest(X_train, data=train.data, ntree=10, mtry=3)
-table(predict(train_rf), X_train)
-train_rf
-plot(train_rf)
+###RANDOM
 
 
-testPred = predict(train_rf, newdata=new_test, type = 'class')
-#table(testPred, test.data$V1)
-CM = table(testPred, new_test$V1)
-accuracy = (sum(diag(CM)))/sum(CM)
-accuracy
+###PCA###
+X = as.matrix(tfidf_train)
+summary(colSums(X))
+scrub_cols = which(colSums(X) == 0)
+X = X[,-scrub_cols]
 
+pca_train = prcomp(X, scale=TRUE)
+
+# looks like 15 or so summaries get us ~50% of the variation in over 1000 features
+summary(pca_train) 
+
+# Look at the loadings
+pca_simon$rotation[order(abs(pca_simon$rotation[,1]),decreasing=TRUE),1][1:25]
+pca_simon$rotation[order(abs(pca_simon$rotation[,2]),decreasing=TRUE),2][1:25]
+
+
+## Look at the first two PCs..
+# We've now turned each document into a single pair of numbers -- massive dimensionality reduction
+pca_simon$x[,1:2]
+
+plot(pca_simon$x[,1:2], xlab="PCA 1 direction", ylab="PCA 2 direction", bty="n",
+     type='n')
+text(pca_simon$x[,1:2], labels = 1:length(simon), cex=0.7)
+
+# 46 and 48 are pretty close
+# Both about Scottish Amicable
+content(simon[[46]])
+content(simon[[48]])
+
+# 25 and 26 are pretty close
+# Both about genetic testing
+content(simon[[25]])
+content(simon[[26]])
+
+# 10 and 11 pretty close
+# Both about Ladbroke's merger
+content(simon[[10]])
+content(simon[[11]])
